d7d0b66319862891f5c5a2c8c3c29db6f65407ae
ChaseDev <clintonkwabena@gmail.com>
2025-05-23 16:09:26 -0700

    Arguments test add in 2-arguments.js

--- 1-multilanguages.js
+++
@@ -1,7 +0,0 @@
-const firstline = "C is fun"
-const secondline = "Python is cool"
-const thirdline = "JavaScript is amazing"
-
-console.log(firstline)
-console.log(secondline)
-console.log(thirdline)
