f9659a2ca3711b7c5fce572af0dc17081660491b
ChaseDev <clintonkwabena@gmail.com>
2025-05-23 06:32:11 -0700

    Add three lines in 1-multi_languages.js

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+const firstline = "C is fun"
+const secondline = "Python is cool"
+const thirdline = "JavaScript is amazing"
+
+console.log(firstline)
+console.log(secondline)
+console.log(thirdline)
