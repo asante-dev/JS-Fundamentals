0dda5853091a20c00d0196c96578fec859c642ad
ChaseDev <clintonkwabena@gmail.com>
2025-05-24 05:16:54 -0700

    5-to_integer.js file added

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,4 +1,4 @@
 const arg = process.argv[2];
 const num = parseInt(arg);
-if (!isNaN(num)) {console.log('My number:${num}');} 
+if (!isNaN(num)) {console.log(`My number:${num}`);} 
 else {console.log('Not a number');}
