a2998950552791f40a9243d8c597542c1723294d
ChaseDev <clintonkwabena@gmail.com>
2025-05-24 08:30:34 -0700

    8 square file added

---
+++ 8-square.js
@@ -0,0 +1,7 @@
+const arg = process.argv[2];
+const size = parseInt(arg);
+if (isNaN(size)) { console.log ("Missing size");} 
+else {let i = 0;
+while (i < size) {
+  console.log("X".repeat(size));
+  i++; }}
